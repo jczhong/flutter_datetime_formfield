afd068b3e71e726b0594eb0fa9c599ade298d2ed
Junchao Zhong <jczhong115@gmail.com>
2019-11-14 21:59:15 +1300

    - add example

---
+++ example/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_formfield/flutter_datetime_formfield.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String title;
+  final DateTime initialDateTime;
+
+  MyApp({Key key, this.title, this.initialDateTime}) : super();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DateTimeFormField Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: title ?? 'Home Page', initialDateTime: initialDateTime ?? DateTime.now(),),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, @required this.title, @required this.initialDateTime}) : super(key: key);
+
+  final String title;
+  final DateTime initialDateTime;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<FormState> _formKey;
+  DateTime _dateTime;
+
+  @override
+  void initState() {
+    _formKey = GlobalKey<FormState>();
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              DateTimeFormField(
+                initialValue: widget.initialDateTime,
+                label: "Date Time",
+                validator: (DateTime dateTime) {
+                  if (dateTime == null) {
+                    return "Date Time Required";
+                  }
+                  return null;
+                },
+                onSaved: (DateTime dateTime) => _dateTime = dateTime,
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: RaisedButton(
+                  child: Text("Submit"),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Date Time"),
+                            content: Text(_dateTime.toIso8601String()),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- test/flutter_datetime_formfield_test.dart
+++ test/flutter_datetime_formfield_test.dart
@@ -1,17 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/intl.dart';
 
-import '../example/main.dart';
+import '../example/lib/main.dart';
 
 void main() {
   testWidgets('test DateTimFormField', (WidgetTester tester) async {
     DateTime dateTime = DateTime.now();
     String textField = DateFormat("EE, MMM d, yyyy h:mma").format(dateTime);
 
-    await tester.pumpWidget(MyWidget(
-      initialValue: dateTime,
-      label: "Date Time",
-    ));
+    await tester.pumpWidget(MyApp(title: "Home Page", initialDateTime: dateTime,));
 
     final labelFinder = find.text("Date Time");
     final textFieldFinder = find.text(textField);
